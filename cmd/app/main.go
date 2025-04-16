fbd2fe48773402b6847bb1c958d5f94356b30a97
Nawfel <155766747+nawfel-dahmani@users.noreply.github.com>
2025-04-16 22:26:05 +0100

    models

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,34 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"Devenir_dev/cmd/handlers"
 	"Devenir_dev/cmd/database"
+	"Devenir_dev/cmd/handlers"
 	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 const port = ":3000"
-func main (){
+
+func main() {
 	database.InitDB()
 	app := mux.NewRouter()
-	app.Use(middelware)
-	app.HandleFunc("/login", handlers.Login)
-	app.HandleFunc("/Submit", handlers.Submit)
-	app.HandleFunc("/Home", handlers.Home)
-	app.HandleFunc("/Home/profs", handlers.List)
-	app.HandleFunc("/deleteUser", handlers.DeleteUserHandler)
-	app.HandleFunc("/Home/Fiche de voeux",handlers.Fiche_de_voeux)
-	fmt.Println("(http://localhost:3000/login) le serveur est lancer sur ce lien ")
-	
+	/*	app.Use(middelware)
+		app.HandleFunc("/login", handlers.Login)
+		app.HandleFunc("/Submit", handlers.Submit)
+		app.HandleFunc("/Home", handlers.Home)
+		app.HandleFunc("/Home/profs", handlers.List)
+		app.HandleFunc("/deleteUser", handlers.DeleteUserHandler)
+		app.HandleFunc("/Home/Fiche de voeux",handlers.Fiche_de_voeux)
+		fmt.Println("(http://localhost:3000/login) le serveur est lancer sur ce lien ")*/
+	app.HandleFunc("/organigramme", handlers.Orga).Methods("POST")
+	app.HandleFunc("/organigramme", handlers.Orga).Methods("GET")
+
 	err := http.ListenAndServe(port, app)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-  
-
-
-
- 
